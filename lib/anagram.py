d32f454aedc05367c153ad03e830a6e7a8bbe90a
Michael Munga <micknyalle@gmail.com>
2025-05-22 06:05:35 +0300

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        # Sort the original word once
+        sorted_word = sorted(self.word)
+        matches = []
+
+        for w in word_list:
+            if sorted(w.lower()) == sorted_word and w.lower() != self.word:
+                matches.append(w)
+
+        return matches
